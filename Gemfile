43105cbdf7636a322f0cf768a1147ecf51cc2378
Jim Harvey <jimrharvey@gmail.com>
2012-05-16 19:39:11 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'sqlite3'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '2989c073f01ebf21574943a86779295b9bca40bad5c446cfd33ef914de93da85275f6bf66c1bd313b2118676e4a29b85c09046c0beb5177352345e5a6a12ffe9'

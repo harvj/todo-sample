b2426958a86b75f06c148fe6bc703e2392bbb225
Jim Harvey <jimrharvey@gmail.com>
2012-05-22 15:59:37 -0700

    add create action for item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,18 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @item  = Item.new
+  end
+
+  def create
+    @items = Item.all
+    @item = Item.new(params[:item])
+    if @item.save
+      flash[:notice] = "Nice."
+    else
+      flash[:notice] = "nope."
+    end
+    redirect_to :index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Todo::Application.routes.draw do
 
   resources :items
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -51,13 +48,4 @@ Todo::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end

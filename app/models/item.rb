06003b3ef582b01c97d9248f33a8ffc7f9b1215d
Jim Harvey <jimrharvey@gmail.com>
2012-06-05 14:09:49 -0700

    allow items to go from active to completed and reverse

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,20 @@
 class Item < ActiveRecord::Base
   attr_accessible :description
-
   validates_presence_of :description
+
+  state_machine :initial => :active do
+
+    event :complete do
+      transition :active => :completed
+    end
+
+    event :restart do
+      transition :completed => :active
+    end
+
+  end
+
+  def initialize
+    super()
+  end
 end
